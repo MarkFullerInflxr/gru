c924ba9a389e800d1c5bbb640f897bf199e840a1
Mark Fuller <mark.fuller@influxer.com>
2025-05-16 21:18:46 -0700

    add to map

--- slice.go
+++ slice.go
@@ -6,3 +6,11 @@ func Map[T, U any](things []T, fn func(T) U) (res []U) {
 	}
 	return res
 }
+
+func ToMap[K comparable, V any](things []V, fn func(V) K) map[K]V {
+	res := make(map[K]V)
+	for _, t := range things {
+		res[fn(t)] = t
+	}
+	return res
+}
